69d418d9f26e7349d00ad7a7c122b34ac17f6bc4
fadawar <fadawar@gmail.com>
2016-07-23 19:26:20 +0200

    Add styling from bootstrap and icons from fontawesome. Add export component.

--- lib/export/export_component.dart
+++ lib/export/export_component.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'dart:html';
+import 'package:intl/intl.dart';
 
 import 'package:angular2/core.dart';
 
@@ -10,9 +11,12 @@ import 'package:negation_marker/article.dart';
     selector: 'export-component',
     templateUrl: 'export_component.html'
 )
-class ExportComponent {
+class ExportComponent implements OnInit {
   final ArticleService _articleService;
   Article article;
+  String filename = "file.xml";
+  String url;
+  String date = new DateFormat('yyyy-MM-dd').format(new DateTime.now());
 
   ExportComponent(this._articleService);
 
@@ -24,13 +28,17 @@ class ExportComponent {
   void exportToXML() {
     String text = '''
 <?xml version="1.0" encoding="UTF-8"?>
+<meta>
+  <url>$url</url>
+  <created>$date</created>
+</meta>
 <article>
   ${article.currentContent}
 </article>''';
     Blob file = new Blob([text], 'text/xml');
     AnchorElement a = new AnchorElement(
         href: Url.createObjectUrlFromBlob(file));
-    a.download = "file.xml";
+    a.download = filename;
     a.click();
   }
 }
